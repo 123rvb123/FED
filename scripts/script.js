0e7b024fd653bf365f6ae773a5cad9a07ba8c05e
123rvb123 <70639520+123rvb123@users.noreply.github.com>
2020-09-22 16:02:49 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,25 @@
 
 var hamburgerMenu = document.querySelector("nav .hamburger");
 var sidebar = document.querySelector("div .sidebarmenu");
+var hamburgerMenuSideBar = document.querySelector("nav .hamburgerinside");
 
 hamburgerMenu.addEventListener("click", toggleMenu);
+hamburgerMenuSideBar.addEventListener("click", toggleMenu);
 
 
-function toggleMenu() {
-	sidebar.classList.toggle("sidebarmenu");
-	side.remove();
-}
+// function toggleMenu() {
+// 	sidebar.classList.toggle("sidebarmenu");
+// }
 
 // function toggleMenu() {
 // 	var sidebar = document.querySelector("div .sidebarmenu");
 // 	sidebar.remove();
 // }
+
+function toggleMenu() {
+ if (sidebar.style.display === "none") {
+    sidebar.style.display = "block";
+  } else {
+    sidebar.style.display = "none";
+  }
+}
